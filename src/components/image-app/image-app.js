b7c455819e9af5b6c2736a80ec57200b1dbe236e
gnkatchmar <gunk55@msn.com>
2016-12-06 20:12:32 -0800

    1206-2010 acheived bonus view selector

--- src/components/image-app/image-app.js
+++ src/components/image-app/image-app.js
@@ -13,5 +13,5 @@ function controller() {
       desc: 'The pygmy rabbit (Brachylagus idahoensis) is a North American rabbit, and is one of only two rabbit species in America to dig its own burrow. The pygmy rabbit is the world\'s smallest leporid, with mean adult weights from 375 to about 500 grams, and a body length from 23.5 to 29.5 centimeters; females are slightly larger than males.'
 	 }
   ];
-
+  this.views = ['text-only', 'thumbnail', 'full-image'];
 }
