fc43213a8669ae5bc2d13aaab63bb8ac637f63c8
David Chase <dchasepdx@gmail.com>
2016-12-07 14:17:49 -0800

    restructuring file system

---
+++ server/test/image.api.test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = chai.assert;
+chai.use(chaiHttp);
+
+const connection = require('../lib/setup-mongoose');
+const app = require('../lib/app');
+
+describe('image api', () => {
+  before(done => {
+    const 
+  })
+})
