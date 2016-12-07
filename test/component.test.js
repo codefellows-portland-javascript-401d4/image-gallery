2636e54c906f3bb7e5c33f20c2aaed5b85038855
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-12-07 09:43:32 -0800

    Initial file setup

---
+++ test/component.test.js
@@ -0,0 +1,13 @@
+// const { assert } = chai;
+
+// describe('component', () => {
+
+//   beforeEach(angular.mock.module('components'));
+
+//   const fn = angular.mock.inject
+//   (function(_$componentCompiler_) {
+//     $controller = _$controller_;
+//   });
+
+//   beforeEach(fn);
+// });
