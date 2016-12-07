c8021ab8124644fa73ba22f91adb51f17de42ec6
nsmeds <niilosmeds@gmail.com>
2016-12-06 16:28:24 -0800

    Display components are displaying.

---
+++ src/components/image-app/image-app.js
@@ -0,0 +1,14 @@
+import template from './image-app.html';
+
+export default {
+    template,
+    controller
+};
+
+function controller() {
+    this.image = {
+            title: 'Elephant',
+            description: 'An adorable baby elephant.',
+            url: 'http://images.nationalgeographic.com/wpf/media-live/photos/000/002/cache/baby-asian-elephant_227_600x450.jpg'
+        };
+}
