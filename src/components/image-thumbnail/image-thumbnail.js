2fdda991d57baac89a0718748a304b05e30f1fbb
David Chase <dchasepdx@gmail.com>
2016-12-07 09:15:39 -0800

    image gallery done

---
+++ src/components/image-thumbnail/image-thumbnail.js
@@ -0,0 +1,14 @@
+import template from './image-thumbnail.html';
+import styles from'./image-thumbnail.css';
+
+export default {
+  template,
+  bindings: {
+    img: '='
+  },
+  controller
+};
+
+function controller() {
+  this.styles = styles;
+}
