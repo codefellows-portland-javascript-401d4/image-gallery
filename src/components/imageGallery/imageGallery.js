7e6b31963c1dc8e696c2ea9dda7f4b4ec7770277
JPGothro <jpgothro@gmail.com>
2016-12-07 14:00:28 -0800

    moved css to components

--- src/components/imageGallery/imageGallery.js
+++ src/components/imageGallery/imageGallery.js
@@ -1,9 +1,11 @@
 
 import template from './imageGallery.html';
+import styles from './imageGallery.css';
 
 export default {
     template,
     bindings: {
         image: '='
-    }
+    },
+    controller () {this.styles = styles;}
 };
--- src/components/imageThumbnail/imageThumbnail.js
+++ src/components/imageThumbnail/imageThumbnail.js
@@ -1,9 +1,11 @@
 
 import template from './imageThumbnail.html';
+import styles from './imageThumbnail.css';
 
 export default {
     template,
     bindings: {
         image: '='
-    }
+    },
+    controller (){this.styles = styles;}
 };
