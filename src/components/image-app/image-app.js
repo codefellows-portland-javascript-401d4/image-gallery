a5215982d42680e8af5e9760b89be759fd72ba3d
nsmeds <niilosmeds@gmail.com>
2016-12-06 16:55:34 -0800

    Added select menu

--- src/components/image-app/image-app.js
+++ src/components/image-app/image-app.js
@@ -7,8 +7,9 @@ export default {
 
 function controller() {
     this.image = {
-            title: 'Elephant',
-            description: 'An adorable baby elephant.',
-            url: 'http://images.nationalgeographic.com/wpf/media-live/photos/000/002/cache/baby-asian-elephant_227_600x450.jpg'
-        };
+        title: 'Elephant',
+        description: 'An adorable baby elephant.',
+        url: 'http://images.nationalgeographic.com/wpf/media-live/photos/000/002/cache/baby-asian-elephant_227_600x450.jpg'
+    };
+    this.selected = 'detail';
 }
