01880a25f0112f3614f25960720fa025ea5ac18e
Gloria Guy <totallygloria@gmail.com>
2016-12-07 20:39:26 -0800

    webpack wasn't building properly, used Marty's code to see if it fixed the camel to kabob case.

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,20 @@
 import angular from 'angular';
+import camelcase from 'camelcase';
+import path from 'path';
+
+
+const context = require.context(
+  './', // this directory
+  true, // include subdirectories
+  /^\.\/(?!index).+?\.js$/ // regex match any .js except this one
+);
 
 const module = angular.module('components', []);
 
-module.component(imageDetail, './image-detail/image-detail.js');
-module.component(imageThumb, './image-thumb/image-thumb.js');
-module.component(imageGallery, './image-gallery/image-gallery.js');
-
+context.keys().forEach(key => {
+  // convert kabob-case to camelCase
+  const name = camelcase(path.basename(key, '.js'));
+  module.component(name, context(key).default);
+});
 
 export default module.name;
