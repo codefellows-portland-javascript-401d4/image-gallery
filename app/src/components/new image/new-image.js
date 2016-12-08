79a8fda7275e147554a4929f93d7e437cd77adba
gnkatchmar <gunk55@msn.com>
2016-12-07 17:36:42 -0800

    1207-1735 add new image component

---
+++ app/src/components/new image/new-image.js
@@ -0,0 +1,33 @@
+import template from './new-image.html';
+// import styles from './new-image.css';
+
+export default {
+  template,
+  bindings: {
+    add: '<'
+  },
+  controller
+};
+
+function controller() {
+  // this.styles = styles;
+
+  this.reset = () => {
+    this.title = '';
+    this.url = '';
+    this.desc = '';
+  };
+
+  this.reset();
+
+  this.addNew = () => {
+    this.add({
+      title: this.title,
+      url: this.url,
+      desc: this.desc
+    });
+        // clear out controls so
+        // next pirate can be added
+    this.reset();
+  };
+}
