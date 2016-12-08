aa3ed6232809a40d035d0ba8031e30e3969a4880
Gloria Guy <totallygloria@gmail.com>
2016-12-07 19:16:29 -0800

    Made a basic angular app which takes components as dependencies and an index page that is linked to the app.

---
+++ src/app.js
@@ -0,0 +1,7 @@
+/** Created by Gloria Anholt on 12/6/16. **/
+
+import angular from 'angular';
+import css from './css';
+import components from './components';
+
+angular.module('imageGallery', [components]);
