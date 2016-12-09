270bbfdc398af3dc8b14e770f52b0d9714a08479
nsmeds <niilosmeds@gmail.com>
2016-12-08 16:03:54 -0800

    changed path.resolve in server/lib/app.js to properly reference build dir, but it hasn't solved my problem

--- server/lib/app.js
+++ server/lib/app.js
@@ -3,7 +3,7 @@ const app = express();
 const errorHandler = require('./error-handler');
 const images = require('./routes/images');
 const path = require('path');
-const buildPath = path.resolve( __dirname + '../build' );
+const buildPath = path.resolve( __dirname + '/../build' );
 
 app.use((req, res, next) => {
     const url = '*';
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+console.log(buildPath);
 app.use(express.static(buildPath));
 app.use('/api/images', images);
 app.use(errorHandler);
