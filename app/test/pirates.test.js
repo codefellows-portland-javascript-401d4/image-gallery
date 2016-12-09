cfcdf1b151b3e61002f653b0de714557754368c5
martypdx <marty@martypdx.com>
2016-12-09 10:18:26 -0800

    add eslint and remove pirate test

--- app/test/pirates.test.js
+++ app/test/pirates.test.js
@@ -4,13 +4,21 @@ describe( 'pirates component', () => {
 
     // to use before, instead of beforeEach, this is required:
     angular.mock.module.sharedInjector();
+
+    // tell angular mock we want to use components module
+    // notice we are using before, not beforeEach
     before(angular.mock.module('components'));
     
+    // get a reference to the $componentController, which we
+    // can then use to create component instances.
+    // In this case, we only are creating in once, via before
     let $component = null;
     before(angular.mock.inject($componentController => {
         $component = $componentController;
     }));
 
+    // use a suite to create one component, and then run a series
+    // of tests, in order, against that component
     describe('create component', () => {
 
         const pirates = [
@@ -19,13 +27,19 @@ describe( 'pirates component', () => {
         ];
 
         const pirate = { name: 'zoro', rank: 'swordsman' };
+        const _id = 123;
         
         const pirateService = {
             get() {
                 return Promise.resolve(pirates);
             },
             add(pirate) {
+                pirate._id = _id;
                 return Promise.resolve(pirate);
+            },
+            remove(pirateId) {
+                assert.equal(pirateId, _id);
+                return Promise.resolve(true);
             }
         };
 
@@ -35,7 +49,6 @@ describe( 'pirates component', () => {
         });
 
         it( 'loads pirates', done => {
-
             assert.isOk(component.loading);
 
             setTimeout(() => {
@@ -53,6 +66,16 @@ describe( 'pirates component', () => {
             setTimeout(() => {
                 assert.equal(pirates.length, 3);
                 assert.equal(pirates[2], pirate);
+                done();
+            });
+        });
+
+        it('removes pirate', done => {
+            component.remove(pirate);
+
+            setTimeout(() => {
+                assert.equal(pirates.length, 2);
+                assert.notInclude(pirates, pirate);
                 done();
             });
         });
