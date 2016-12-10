51fe0856ef903fadc4ef754f5d33ee1d268a757a
DrewStock <andrewdstock@gmail.com>
2016-12-10 14:20:19 -0800

    added 'cors' module to server, added styling to buttons in app

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
 const errorHandler = require('./error_handler');
+const cors = require('cors');
 
 const images = require('./routes/images');
 
-app.use((req, res, next) => {
-  const url = '*';
-  res.header('Access-Control-Allow-Origin', url);
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
 
 app.use(express.static('./public'));
 
