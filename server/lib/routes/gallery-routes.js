8ac898d004dde2be88fe1dff43daee94ae5faec9
Gloria Guy <totallygloria@gmail.com>
2016-12-09 16:12:57 -0800

    Adds the mongoose logic for getting all, getting by ID, and posting a new image to the gallery. Untested.

--- server/lib/routes/gallery-routes.js
+++ server/lib/routes/gallery-routes.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const gallery = require('../models/gallery');
+const Gallery = require('../models/gallery');
 
 const router = express.Router();
 
 router
   .get('/', (req, res, next) => {
-    next();
+    Gallery.find()
+      .lean()
+      .then(images => res.send(images))
+      .catch(next);
   })
   .get('/:id', (req, res, next) => {
-    next();
+    Gallery.findById(req.params.id)
+      .lean()
+      .then(image => res.send(image))
+      .catch(next);
   })
   .post('/', bodyParser, (req, res, next) => {
-    next();
+    new Gallery(req.body).save();
+      .then(saved => req.send({
+      message: `${req.body.name} successfully saved.`
+      }))
+      .catch(next);
 });
