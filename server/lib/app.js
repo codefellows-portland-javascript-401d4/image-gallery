aeaaf7c5ad0aca5ad337f223122707fcb6eb05b4
Andrew Beers <andybeers@gmail.com>
2016-12-11 21:08:58 -0800

    Added morgan

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const errorHandler = require('./error-handler');
+const morgan = require('morgan');
 
-app.use();
+app.use(morgan('dev'));
+
+
 app.use(errorHandler);
 
 module.exports = app;
