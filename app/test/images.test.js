21ed4874d44bc6477630151f656b34b93e2e2768
Tim Combs <tim00combs@gmail.com>
2016-12-12 04:24:02 -0800

    add tests to images.test.js - testing images loads, can add & remove images, update .eslintrc to use angular & chai globally - don't have to require it in

---
+++ app/test/images.test.js
@@ -0,0 +1,90 @@
+//testing images component
+
+describe('in the images component', () => {
+  const {assert} = chai;
+
+  angular.mock.module.sharedInjector(); //use before instead of beforeEach
+
+  before(angular.mock.module('components')); //bring in components module
+
+  //$componentController allows creation of component instances
+  let $component = null;
+  before(angular.mock.inject($componentController => {
+    $component = $componentController;
+  }));
+
+  
+  describe('create a component to test against', () => {
+
+    const images = [
+      {
+        name: 'first-image',
+        description: 'the first image',
+        url: 'http://www.first-image.mock'
+      },
+      {
+        name: 'second-image',
+        description: 'the second image',
+        url: 'http://www.second-image.mock'
+      }
+    ];
+
+    const thirdImage = {
+      name: 'third-image',
+      description: 'the third image',
+      url: 'http://www.third-image.mock'
+    };
+
+    const _id = 123;
+
+    const imageService = {
+      get() {
+        return Promise.resolve(images);
+      },
+      add(image) {
+        image._id = _id;
+        return Promise.resolve(images);
+      },
+      remove(imageId) {
+        assert.equal(imageId, _id);
+        return Promise.resolve(true);
+      }
+    };
+
+    let component = null;
+    before(() => {
+      component = $component('images', {imageService});
+    });
+
+    it('loads images', done => {
+      assert.isOk(component.loading);
+
+      setTimeout(() => {
+        assert.equal(component.images, images);
+        assert.isNotOk(component.loading);
+        done();
+      });
+    });
+
+    it('adds an image', done => {
+      component.add(thirdImage);
+
+      setTimeout(() => {
+        assert.equal(images.length, 3);
+        assert.equal(images[2], thirdImage);
+        done();
+      });
+    });
+
+    it('removes an image', done => {
+      component.remove(thirdImage);
+
+      setTimeout(() => {
+        assert.equal(images.length, 2);
+        assert.notInclude(images, thirdImage);
+        done();
+      });
+    });
+
+  });
+});
