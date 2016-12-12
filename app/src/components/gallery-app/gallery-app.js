d9f92be2ba2cda427d31026bc018af178e58ea09
Andrew Beers <andybeers@gmail.com>
2016-12-12 13:46:13 -0800

    Wired front end to back end

--- app/src/components/gallery-app/gallery-app.js
+++ app/src/components/gallery-app/gallery-app.js
@@ -3,13 +3,20 @@ import template from './gallery-app.html';
 export default {
   template,
   controller
-  // controllerAs: 'app'
 };
 
-function controller() {
-  this.img = {
-    title: 'Bunny',
-    url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg',
-    description: 'Some sort of bunny.'
-  };
+controller.$inject = ['imageService'];
+
+function controller(imgSvc) {
+  imgSvc.get()
+    .then(images => {
+      this.img = images[0];
+    });
 }
+
+
+// this.img = {
+//     title: 'Bunny',
+//     url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg',
+//     description: 'Some sort of bunny.'
+//   };
