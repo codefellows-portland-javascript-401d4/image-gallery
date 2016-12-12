9645cc2df915fd4c622e3fa561bdaf7f20e7faef
Gloria Guy <totallygloria@gmail.com>
2016-12-12 13:58:52 -0800

    Adds mocked angular $http test file. Passes GET tests.
    Pulls in my galleryService and the built-in $httpBackend mocking
    functionality to test my service without actually hitting the back
    end server. Be careful to have the expected route and the apiUrl
    route match in usage, not just as strings. Passes first test.

---
+++ app/test/gallery-service.test.js
@@ -0,0 +1,40 @@
+const assert = chai.assert;
+
+
+describe('Gallery Service', () => {
+
+  beforeEach(angular.mock.module('services', { apiUrl: '/api/gallery' }));
+
+  let $httpBackend = null;
+  let galleryService = null;
+
+  beforeEach(angular.mock.inject((_galleryService_, _$httpBackend_) => {
+    $httpBackend = _$httpBackend_;
+    galleryService = _galleryService_;
+  }));
+
+  afterEach(() => {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+
+  it('gets images from a mocked backend', done => {
+
+    const images = [{"title":"An image","url":"http://www.image.com","description":"an image"}];
+
+    $httpBackend
+      .expectGET('/api/gallery/')  // what http VERB and url are we expecting?
+      .respond(images);  // mock a response, set on the .data property of $http response object
+
+    galleryService.get()
+      .then(allImages => {
+        assert.deepEqual(allImages, images);
+        done();
+      })
+      .catch(done);
+
+    $httpBackend.flush();
+  });
+
+});
