c5eec5f1e9540a05dd487c6e1aa842dd7f0761f5
risafletcher <risa.fletcher@gmail.com>
2016-12-12 07:53:58 -0800

    environment reconfigured for the most part

---
+++ app/src/app.js
@@ -0,0 +1,8 @@
+import angular from 'angular';
+import components from './components';
+import services from './services';
+
+const app = angular.module('imageGallery', [components, services]);
+const dev = 'http://localhost:3000/api';
+
+app.value('apiUrl', dev);
---
+++ app/src/components/thumbnail/thumbnail-app.js
@@ -0,0 +1,17 @@
+import template from './thumbnail-app.html';
+import styles from './thumbnail-app.css';
+
+export default {
+  template,
+  bindings: {
+    bunny: '='
+  },
+  controller
+};
+
+function controller() {
+  this.styles = styles;
+  this.thumbnail = {
+    src: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg'
+  };
+}
