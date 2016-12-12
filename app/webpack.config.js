ec11dfa86eef6efc6764875c0ea453ba015aad54
gnkatchmar <gunk55@msn.com>
2016-12-12 14:20:48 -0800

    button colors partials added

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/main.js',
   output: {
     path: '../server/public',
     filename: 'main.js'
@@ -12,7 +12,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-    new ExtractTextPlugin('main.css')
+        // new ExtractTextPlugin('main.css')
   ],
   module: {
     preLoaders: [{
@@ -25,14 +25,18 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['es2015'] 
+        cacheDirectory: true,
+                // plugins: ['transform-runtime']
       }
     }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')	
+      test: /\.scss$/,
+      loader: 'style-loader!css-loader?sourceMap!sass-loader?sourceMap'	
     }, {
       test: /\.html$/,
       loader: 'html-loader'	
     }]
+  },
+  sassLoader: {
+    includePaths: ['./src/scss/partials']
   }
 };
