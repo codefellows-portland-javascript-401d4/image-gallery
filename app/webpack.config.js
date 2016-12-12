6cb14169920854155e5a62a8fa255e8367d278ed
Christopher Bruner <chrbruner9@gmail.com>
2016-12-12 15:18:41 -0800

    converted all css files to scss, created partials files to modularize repeating css structures

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
-        new ExtractTextPlugin('main.css')
+        // new ExtractTextPlugin('main.css')
     ],
     module: {
         preLoaders: [{
@@ -26,16 +26,21 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
-                presets: ['es2015'] //,
-                // cacheDirectory: true,
+                // presets: ['es2015'],
+                cacheDirectory: true,
                 // plugins: ['transform-runtime']
             }
         }, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')	
+            // test: /\.css$/,
+            // loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            test: /\.scss$/,
+            loader:	'style-loader!css-loader?sourceMap!sass-loader?sourceMap'
         }, {
             test: /\.html$/,
             loader: 'html-loader'	
         }]
+    },
+    sassLoader: {
+        includePaths: ['./src/scss/partials']
     }
 };
