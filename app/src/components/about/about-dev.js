12514085bd33a64a25c4b9f9e05ae736cfdc8e0b
Gloria Guy <totallygloria@gmail.com>
2016-12-13 15:39:49 -0800

    Adds an empty parent About page with child App and Dev abouts
    The links work to change the subview! Simply putting in an empty
    <ui-view> tag will be filled in by the ui-router -- doesn't require
    logic to handle the name spaces.
    
    No stylin on new links at all.

---
+++ app/src/components/about/about-dev.js
@@ -0,0 +1,17 @@
+export default {
+  template:
+  `    <h2>About the Developer</h2>
+    <p>Gloria Anholt is a Full-Stack JavaScript developer, specializing in
+        the MEAN (Mongo, Express, Angular, NodeJS) stack. A long time Linux
+        loving nerd, she also codes in Python, when possible. When not at a
+        keyboard, Gloria is generally in her overgrown vegetable and herb
+        garden, or building custom mason-jar shelving in her woodshop.
+    </p>
+    <p>To play with the code yourself, checkout
+        <a href="https://github.com/GloriaAnholt/image-gallery/">my
+            image-gallery repo on Github</a>.
+        A full suite of tests for the server are found in server/test/,
+        somewhat less comprehensive tests are available for the front end
+        in app/test.
+    </p>`
+};
---
+++ app/src/components/about/about.js
@@ -0,0 +1,7 @@
+export default {
+  template:
+  `<a ui-sref="about.app">About the App</a>
+   <a ui-sref="about.dev">About the Developer</a>
+   <section><ui-view></ui-view></section>
+  `
+};
