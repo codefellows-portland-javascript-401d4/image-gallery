7674d2367b730c657f1e4661d38777611eda596f
mizutombo <thomas.shultz52@gmail.com>
2016-12-13 02:21:18 -0800

    scss code for lab3 ... initial cut

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-    new ExtractTextPlugin('main.css')
+    // new ExtractTextPlugin('main.css')
   ],
   module: {
     preLoaders: [{
@@ -30,11 +30,17 @@ module.exports = {
                 // plugins: ['transform-runtime']
       }
     }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract(
+        'style-loader',
+        'css-loader?sourceMap!sass-loader?sourceMap'
+      )
     }, {
       test: /\.html$/,
       loader: 'html-loader'
     }]
+  },
+  sassLoader: {
+    includePaths: ['./src/scss/partials']
   }
 };
