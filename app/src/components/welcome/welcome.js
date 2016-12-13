20f5da3dadbef83ad0f59a6232fdace7885a1247
DrewStock <andrewdstock@gmail.com>
2016-12-13 13:15:33 -0800

    added routes.js with routes (home, images)

---
+++ app/src/components/welcome/welcome.js
@@ -0,0 +1,8 @@
+export default {
+  template: `
+  <h2>Welcome to the Bunny Gallery!</h2>
+  <section>
+    <ui-view>Bunnies! Bunnies! Bunnies!</ui-view>
+  </section>
+  `
+};
---
+++ app/src/routes.js
@@ -0,0 +1,18 @@
+routes.inject = ['$stateProvider', '$urlRouterProvider'];
+
+export default function routes($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state({
+    name: 'welcome',
+    url: '/',
+    component: 'welcome'
+  });
+
+  $stateProvider.state({
+    name: 'images',
+    url: '/images',
+    component: 'images'
+  });
+
+  $urlRouterProvider.otherwise('/');
+}
