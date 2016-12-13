ace2099ba6ca4b2ae54250aaf9916f58cbd6c00c
Gloria Guy <totallygloria@gmail.com>
2016-12-12 17:36:24 -0800

    Removes configuration from karma.

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -36,17 +36,5 @@ module.exports = function(config) {
     // Concurrency level: how many browser should be started simultaneous
     concurrency: Infinity,
 
-    if (process.env.TRAVIS) {
-    configuration.customLaunchers = {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    };
-    configuration.broswers = ['Chrome_travis_ci'];
-    configuration.singleRun = true;
-  }
-  config.set(configuration);
-
   });
 };
