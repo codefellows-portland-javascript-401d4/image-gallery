19eef405e827fe16d2940c8bb1d26ce0a039234f
Nathan Pickard <nathan.pickard87@gmail.com>
2016-12-12 16:39:33 -0800

    Adding Services folder and files and server files

---
+++ server/lib/setup-mongoose.js
@@ -0,0 +1,26 @@
+const mongoose = require( 'mongoose' );
+const dbURI = process.env.MONGOD_URI || 'mongodb://localhost/images';
+
+mongoose.Promise = Promise;
+mongoose.connect( dbURI );
+
+mongoose.connection.on('connected', function () {
+  console.log( 'Mongoose default connection open to ' + dbURI );
+});
+
+mongoose.connection.on('error', function (err) {
+  console.log( 'Mongoose default connection error: ' + err );
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log( 'Mongoose default connection disconnected' );
+});
+
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log( 'Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
+module.exports = mongoose.connection;
