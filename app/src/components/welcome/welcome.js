a368c785444a90a6d5e98004895ebb07f3d24cb1
gnkatchmar <gunk55@msn.com>
2016-12-13 15:20:56 -0800

    Essentially Done

--- app/src/components/welcome/welcome.js
+++ app/src/components/welcome/welcome.js
@@ -1,5 +1,8 @@
 export default {
   template: `
-    <h1>Very Impressive Image Gallery Welcome Page</h1>
+    <h1>Rabbit Image Gallery Welcome Page</h1>
+		<br>
+		<img src=http://mentalfloss.com/sites/default/legacy/wp-content/uploads/2008/10/435HolyGrail.jpg>
+
 `
 };
