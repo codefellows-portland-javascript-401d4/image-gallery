3e40b56449bc505239525e2410ba777b0b1532c8
nsmeds <niilosmeds@gmail.com>
2016-12-13 12:40:44 -0800

    updated some more config stuff

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const cssExtract = new ExtractTextPlugin('main.css');
 
 module.exports = {
     entry: './src/app.js',
@@ -12,7 +13,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }), 
-        new ExtractTextPlugin('main.css')
+        cssExtract
     ],
     module: {
         preLoaders: [{
@@ -30,7 +31,7 @@ module.exports = {
             }
         },     {
             test: /\.scss$/,
-            loader: ExtractTextPlugin.extract(
+            loader: cssExtract.extract(
                 'style-loader',
                 'css-loader?sourceMap!sass-loader?sourceMap'
             )    
