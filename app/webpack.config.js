e597e7ab769c20c13381d90ddf3e1498c95f7c54
Gloria Guy <totallygloria@gmail.com>
2016-12-13 10:52:34 -0800

    Updates css text extracting for scss.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const cssExtract = new ExtractTextPlugin('main.css');
+
 module.exports = {
   entry: './src/app.js',
   output: {
@@ -12,7 +14,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-    new ExtractTextPlugin('main.css')
+    cssExtract
   ],
   module: {
     preLoaders: [{
@@ -32,7 +34,8 @@ module.exports = {
       loader: ExtractTextPlugin.extract('style-loader!css-loader')
     }, {
       test: /\.scss$/,
-      loader: 'style-loader!css-loader?sourceMap!sass-loader?sourceMap'
+      loader: cssExtract.extract('style-loader',
+        'css-loader?sourceMap!sass-loader?sourceMap')
     }, {
       test: /\.html$/,
       loader: 'html-loader'
