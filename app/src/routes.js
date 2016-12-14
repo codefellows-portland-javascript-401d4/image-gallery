47eb5983d9cfb94e5f03495b01327d0e3307f20c
nsmeds <niilosmeds@gmail.com>
2016-12-14 15:09:32 -0800

    Reorganized image dirs

--- app/src/routes.js
+++ app/src/routes.js
@@ -9,6 +9,12 @@ export default function routes($stateProvider, $urlRouterProvider) {
     });
 
     $stateProvider.state({
+        name: 'albums',
+        url: '/',
+        component: 'albums'
+    });
+
+    $stateProvider.state({
         name: 'images',
         url: '/images',
         component: 'images',
