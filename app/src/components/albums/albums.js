32d75e48498c460d191b655d24a7943058ae3bbe
nsmeds <niilosmeds@gmail.com>
2016-12-14 15:47:39 -0800

    added album service

---
+++ app/src/components/albums/albums.js
@@ -0,0 +1,20 @@
+import template from './albums.html';
+import styles from './albums.scss';
+export default {
+    template,
+    controller
+};
+
+controller.$inject = ['albumService', '$state'];
+function controller(albums, $state) {
+    this.view = 'detail';
+    this.styles = styles;
+
+    this.updateView = () => {
+        $state.go($state.current.name, { view: this.view });
+    };
+
+    albums.get().then(albums => {
+        this.albums = albums;
+    });
+}
