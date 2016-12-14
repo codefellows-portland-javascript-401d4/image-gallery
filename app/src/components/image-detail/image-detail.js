9c1e97e7ae244d0ff0a0f9919c8593ac79207c9f
nsmeds <niilosmeds@gmail.com>
2016-12-14 09:51:09 -0800

    removed unused/redundant add function from image-detail component

--- app/src/components/image-detail/image-detail.js
+++ app/src/components/image-detail/image-detail.js
@@ -4,20 +4,12 @@ export default {
     template,
     bindings: {
         image: '=',
-        add: '<',
         remove: '<'
     },
     controller
 };
 
 function controller() {
-    this.addNew = () => {
-        this.add({
-            title: this.title,
-            description: this.description,
-            url: this.url
-        });
-    };
 
     this.delete = () => {
         this.remove(this.image);
