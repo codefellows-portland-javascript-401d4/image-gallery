b56b61205aa21d5e365c1da8f035065acd9186b3
Nathan Hugon <nthugon@gmail.com>
2016-12-13 17:31:18 -0800

    routes set up/more styling added

---
+++ client/src/components/developer/developer.js
@@ -0,0 +1,12 @@
+import template from './developer.html';
+import styles from './developer.scss';
+
+export default {
+    template,
+    controller
+};
+
+
+function controller() {
+    this.styles = styles;
+}
---
+++ client/src/components/project/project.js
@@ -0,0 +1,12 @@
+import template from './project.html';
+import styles from './project.scss';
+
+export default {
+    template,
+    controller
+};
+
+
+function controller() {
+    this.styles = styles;
+}
