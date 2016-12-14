5ae65375e1a4d3dd0e19183b2a7f62e679727f00
Mugsy Carter <mugsycarter@gmail.com>
2016-12-14 11:44:26 -0800

    see goat images button added dynamically in home-detail

--- app/src/components/home-detail/home-detail.js
+++ app/src/components/home-detail/home-detail.js
@@ -6,30 +6,34 @@ export default {
 	controllerAs: 'homeDetail'  
 };
 
-controller.$inject = ['$scope'];
+controller.$inject = ['$scope', '$location'];
 
 function controller($scope) {
 	this.loading = true;
 
 	this.checkAnimal = function(){
-		console.log('animal choice is ', $scope.animalChoice);
 		const patt = /goats?/i;
 		var result = patt.test($scope.animalChoice);
 		if (result){
-			console.log('goats');
 			$scope.responseText = 'Good choice!  We have lots of pictures of goats!';
+			$scope.picturesButton = true;
 		}
 		else {
-			console.log('no');
 			var arr = $scope.animalChoice.split('');
 			var lastLetter = arr.pop();
-			console.log(lastLetter);
 			if(lastLetter === 's' || lastLetter === 'S'){
 				$scope.responseText = 'Sorry, we do not have any pictures of '+$scope.animalChoice+'.';
 			}
 			else{
 				$scope.responseText = 'Sorry, we do not have any pictures of '+$scope.animalChoice+'s.';
 			}
+			$scope.picturesButton = false;
 		}
 	};
+
+	this.goToImages = function($location){
+		console.log('button clicked.  path is ', $location.path);
+		$location.path('images'); 
+	};
+
 }
