5613c2cd3a418928c0106e59f18429490440a75a
Christopher Bruner <chrbruner9@gmail.com>
2016-12-13 20:00:41 -0800

    removed firefox from the karma config for configuration browsers

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
                 flags: ['--no-sandbox']
             }
         };
-        configuration.browsers = ['Chrome_travis_ci', 'Firefox'];
+        configuration.browsers = ['Chrome_travis_ci']; //, 'Firefox'];
         configuration.singleRun = true;
     };
     
