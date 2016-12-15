9b7569890246728a55a93e01c02103822c7361f6
Gloria Guy <totallygloria@gmail.com>
2016-12-14 21:38:15 -0800

    Transitions page to have about links in sidebar
    The about "page" is no longer a navigatable page, as indicated by
    the "abstract: true" property. Instead, it has two child views
    which will display in the main body when they're clicked.
    
    The ui-view on the index page is where the about page hangs it's
    ui-view for it's children to populate. You need both to get the
    templates to load.

--- app/src/routes.js
+++ app/src/routes.js
@@ -11,6 +11,7 @@ export default function routes($stateProvider, $urlRouterProvider) {
 
   $stateProvider.state({
     name: 'about',
+    abstract: true,
     url: '/about',
     component: 'about'
   });
