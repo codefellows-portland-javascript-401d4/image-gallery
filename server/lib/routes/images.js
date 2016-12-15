d544b3e8d8407f27577a12929c8873c93e3819f6
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-12-15 12:14:40 -0800

    Routes added for images

---
+++ server/lib/routes/images.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser').json();
+const Image = require('../models/image');
+
+router
+  .get('/', (req, res, next) => {
+    const query = {};
+
+    Image.find(query)
+      .then(images => res.send(images))
+      .catch(next);
+  })
+
+  .post('/', bodyParser, (req, res, next) => {
+    new Image(req.body).save()
+      .then(saved => res.send(saved))
+      .catch(next);
+  })
+
+  .delete('/:id', (req, res, next) => {
+    Image.findByIdAndRemove(req.params.id)
+      .then(deleted => res.send(deleted))
+      .catch(next);
+  })
+
+  module.exports = router;
+  
