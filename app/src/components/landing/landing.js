8abaac3c85bfed6bddef9ddeda3250a6344dad23
Gloria Guy <totallygloria@gmail.com>
2016-12-15 15:28:05 -0800

    Updates landing welcome message.

--- app/src/components/landing/landing.js
+++ app/src/components/landing/landing.js
@@ -1,7 +1,8 @@
 export default {
   template:
     "<h3>A Simple Gallery App</h3>" +
-    "<p>Click Gallery View to see images full-size</p>" +
-    "<p>Click Thumbnail View to see all images</p>" +
-    "<p>Click Detail View to see one image</p>"
+    "<p>Welcome to the image gallery.</p>" +
+    "<p>Click Gallery View to see images full-size, " +
+    "Thumbnail View to see all images, " +
+    "or Detail View to see one image</p>"
 };
