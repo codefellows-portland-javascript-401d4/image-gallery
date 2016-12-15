2ae4be77fd13fbf5fc1f7224b7059dd24b074884
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-12-15 12:56:05 -0800

    Edited image-manage component

---
+++ app/src/components/image-manage/image-manage.js
@@ -0,0 +1,34 @@
+import template from './image-manage.html';
+
+export default {
+  template,
+  controller
+};
+
+controller.$inject = ['imageService'];
+
+function controller(images) {
+  this.styles = styles;
+  this.views = ['preview', 'detail', 'full'];
+  this.view = this.views[2];
+
+  images.get()
+    .then(images => {
+      this.images = images;
+    });
+
+  this.add = image => {
+    images.add(image)
+      .then(saved => {
+        this.images.push(saved);
+      });
+  };
+
+  this.remove = image => {
+    images.remove(image._id)
+      .then(() => {
+        const index = this.images.indexOf(image);
+        if(index > -1) this.images.splice(index, 1);
+      });
+  };
+}
