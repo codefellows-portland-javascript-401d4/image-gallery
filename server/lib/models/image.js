797617367f964dfd0fff0870e01faa90fee63e17
Gloria Guy <totallygloria@gmail.com>
2016-12-15 16:44:52 -0800

    Fixes typo in model: missing comma.

--- server/lib/models/image.js
+++ server/lib/models/image.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
   title: {type: String, required: true},
   url: {type: String, required: true},
-  description: {type: String, required: true}
+  description: {type: String, required: true},
   album: {
     type: Schema.Types.ObjectId,
     ref: 'Album'
