9bfcdc1a4e0ae71ca140252501f72542d2a68234
Gloria Guy <totallygloria@gmail.com>
2016-12-15 17:54:21 -0800

    Updates app default route and services routes to match API
    Took gallery out of the url since it's not used anymore. Instead,
    adds remaining methods to get albums and images -- front end service
    now has methods which correspond to all of the backend routes.
    
    Name change errors need to be fixed.

--- app/src/services/gallery-service.js
+++ app/src/services/gallery-service.js
@@ -3,26 +3,55 @@ galleryService.$inject = ['$http', 'apiUrl'];
 
 export default function galleryService($http, apiUrl) {
   return {
-    get() {
+    getAlbums() {
       return $http.get(`${apiUrl}/`)
         .then(res => {
           return res.data;
         });
     },
 
-    add(image) {
+    getAlbum(id) {
+      return $http.get(`${apiUrl}/albums/${id}`)
+        .then(res => {
+          return res.data;
+        });
+    },
+
+    addAlbum(album) {
+      return $http.post(`${apiUrl}/albums/`, album)
+        .then(res => {
+          return res.data;
+        });
+    },
+
+    addImage(image) {
       return $http.post(`${apiUrl}/`, image)
         .then(res => {
           return res.data;
         });
     },
 
-    remove(id) {
-      return $http.delete(`${apiUrl}/${id}`)
+    getImage(id) {
+      return $http.get(`${apiUrl}/images/${id}`)
+        .then(res => {
+          return res.data;
+        });
+    },
+
+    updateImage(id) {
+      return $http.put(`${apiUrl}/images/${id}`)
+        .then(res => {
+          return res.data;
+        });
+    },
+
+    removeImage(id) {
+      return $http.delete(`${apiUrl}/images/${id}`)
         .then(res => {
           return res.data;
         });
     }
+
   };
 }
 
