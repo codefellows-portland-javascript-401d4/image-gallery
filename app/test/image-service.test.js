6bebb37be00ee4a613767fed509515d453c03439
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-12-18 17:00:31 -0800

    Image service testing get, post, delete

--- app/test/image-service.test.js
+++ app/test/image-service.test.js
@@ -18,27 +18,68 @@ describe('image service', () => {
     $httpBackend.verifyNoOutstandingRequest();
   });
 
-  it.skip('GETs images', done => {
+  it('GETs all images', done => {
     const images = [a, b, c];
+
+    $httpBackend
+      .expectGET('/api/images')
+      .respond(images);
+
+    imageService
+      .get()
+      .then(allImages => {
+        assert.deepEqual(allImages, images);
+        done();
+      })
+      .catch(done);
+
+    $httpBackend.flush();
   });
 
-  it.skip('POSTs a new image', done => {
+  it('POSTs a new image', done => {
     const image = {
-      title: '',
-      description: '',
-      url: ''
+      title: 'Image-service test image',
+      description: 'Test image for image-service',
+      url: 'http://i.imgur.com/yqtLojN.jpg'
     };
+
+    $httpBackend
+      .expectPOST('/api/images', image)
+      .respond(image);
+
+    imageService
+      .add(image)
+      .then(saved => {
+        assert.deepEqual(saved, image);
+        done();
+      })
+      .catch(done);
+
+    $httpBackend.flush();
   });
 
-  it.skip('DELETEs an image by id', done => {
-    const testImage = {
-      title: '',
-      description: '',
-      url: '',
+  it('DELETEs an image by id', done => {
+    const removed = {
+      title: 'Remove-image test',
+      description: 'Test image for image-remove',
+      url: 'http://i.imgur.com/9oGI5Tz.jpg',
       id: '1234'
     };
     
     const id = 1234;
-  });
 
+    $httpBackend
+      .expectDELETE(`/api/images/${ id }`)
+      .respond(removed);
+
+    imageService
+      .remove(id)
+      .then(deleted => {
+        assert.deepEqual(deleted, removed);
+        done();
+      })
+      .catch(done);
+
+    $httpBackend.flush();
+  });
 });
