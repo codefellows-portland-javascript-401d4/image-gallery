bdab276981d559f6e515da135b7b04c394cd984a
Mugsy Carter <mugsycarter@gmail.com>
2016-12-19 16:29:00 -0800

    extra ui-view tags deleted.

--- app/src/components/about/about.js
+++ app/src/components/about/about.js
@@ -2,5 +2,5 @@
 export default {
 	template: `
     <h2>About Goat Images</h2>
-        <ui-view name="header"><h3>These are a bunch of images of goats, that's it.  Just some pictures of goats. </h3></ui-view>`
+       <h3>These are a bunch of images of goats, that's it.  Just some pictures of goats. </h3>`
 };
--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,7 @@ import './scss/main.scss';
 import components from './components';
 import services from './services';
 import uiRouter from 'angular-ui-router';
+// import defaultRoute from 'angular-ui-router-default';
 import routes from './routes'; 
 
 const app = angular.module('myApp', [components, services, uiRouter]);
