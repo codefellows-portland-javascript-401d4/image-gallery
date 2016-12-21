e8bd0c6437b0ed92ffbd746de2e46e1623bfd88c
gnkatchmar <gunk55@msn.com>
2016-12-21 06:51:53 -0800

    some setup + server test changes-Travis still works?

--- server/test/api-test.js
+++ server/test/api-test.js
@@ -1,73 +1,77 @@
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// const assert = chai.assert;
-// chai.use(chaiHttp);
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = chai.assert;
+chai.use(chaiHttp);
 
-// const connection = require('../lib/setup-mongoose');
-// const app = require('../lib/app');
+process.env.MONGODB_URI = 'mongodb://localhost/test-database-name';
 
-// describe('image', () => {
+const connection = require('../lib/setup-mongoose');
+const app = require('../lib/app');
 
-//   before(done => {
-//     const drop = () => connection.db.dropDatabase(done);
-//     if (connection.readyState === 1) drop();
-//     else {
-//       connection.on('open', drop);
-//     }
-//   });
+describe('image', () => {
 
-//   const request = chai.request(app);
+  before(done => {
+    const drop = () => connection.db.dropDatabase(done);
+    if (connection.readyState === 1) drop();
+    else {
+      connection.on('open', drop);
+    }
+  });
 
-//   const cottontail = {
-//     title: 'Desert Cottontail',
-//     url: 'https://en.wikipedia.org/wiki/File:Sylvilagus_audubonii_2.jpg',
-//     desc: 'The desert cottontail (Sylvilagus audubonii), also known as Audubon\'s cottontail, is a New World cottontail rabbit, and a member of the family Leporidae.'
-//   };
+  const request = chai.request(app);
 
-//   it('/GET all', done => {
-//     request
-//             .get('/api/images')
-//             .then(res => {
-//               assert.deepEqual(res.body, []);
-//               done();
-//             })
-//             .catch(done);
-//   });
+  const cottontail = {
+    title: 'Desert Cottontail',
+    url: 'https://en.wikipedia.org/wiki/File:Sylvilagus_audubonii_2.jpg',
+    desc: 'The desert cottontail (Sylvilagus audubonii), also known as Audubon\'s cottontail, is a New World cottontail rabbit, and a member of the family Leporidae.'
+  };
 
-//   it('/POST', done => {
-//     request
-//             .post('/api/images')
-//             .send(cottontail)
-//             .then(res => {
-//               const image = res.body;
-//               assert.ok(image._id);
-//               cottontail._id = image._id;
-//               cottontail.__v = 0;
-//               done();
-//             })
-//             .catch(done);
+  it('/GET all', done => {
+    request
+            .get('/api/images')
+            .then(res => {
+              assert.deepEqual(res.body, []);
+              done();
+            })
+            .catch(done);
+  });
 
-//   });
+  it('/POST', done => {
+    request
+            .post('/api/images')
+            .send(cottontail)
+            .then(res => {
+              const image = res.body;
+              assert.ok(image._id);
+              cottontail._id = image._id;
+              cottontail.__v = 0;
+              done();
+            })
+            .catch(done);
 
-//   it('/GET by id', done => {
-//     request
-//             .get(`/api/images/${cottontail._id}`)
-//             .then(res => {
-//               const image = res.body;
-//               assert.deepEqual(image, cottontail);
-//               done();
-//             })
-//             .catch(done);
-//   });
+  });
 
-//   it('/GET all after post', done => {
-//     request
-//             .get('/api/images')
-//             .then(res => {
-//               assert.deepEqual(res.body, [ cottontail ]);
-//               done();
-//             })
-//             .catch(done);
-//   });
+	//next two no longer work because changed GETs to curated selection
 
-// });
+  // it('/GET by id', done => {
+  //   request
+  //           .get(`/api/images/${cottontail._id}`)
+  //           .then(res => {
+  //             const image = res.body;
+  //             assert.deepEqual(image, cottontail);
+  //             done();
+  //           })
+  //           .catch(done);
+  // });
+
+  // it('/GET all after post', done => {
+  //   request
+  //           .get('/api/images')
+  //           .then(res => {
+  //             assert.deepEqual(res.body, [ cottontail ]);
+  //             done();
+  //           })
+  //           .catch(done);
+  // });
+
+});
