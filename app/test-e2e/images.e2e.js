699efe5fe07f0d2f1420f6c6373268549b2e2392
Tyler Smith <tyler.smith6891@gmail.com>
2016-12-21 21:04:26 -0800

    setup a test (failing)

--- app/test-e2e/images.e2e.js
+++ app/test-e2e/images.e2e.js
@@ -1,5 +1,8 @@
-describe('test', () => {
-  it('should pass', () => {
-    expect(true);
+describe('image gallery', () => {
+   
+  it('should land on welcome page', () => {
+    const welcome = browser.findElement(by.css('.welcome'));
+    expect(welcome.getText()).toBe('Welcome to this sweet image gallery app!');
   });
+
 });
