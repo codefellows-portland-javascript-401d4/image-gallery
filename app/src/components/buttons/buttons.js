dc1027701c26ba7625c088cc7f8c78433434705a
Gloria Guy <totallygloria@gmail.com>
2016-12-24 14:02:03 -0800

    Moves old image-app into just buttons to toggle image views
    This component had been the heart of the old app, but all it really
    does is toggle between views. Instead, it now is a component which
    is displayed at the top of the 'body' section of the page.
    
    Changed from using a view==='detail' type click to using the ui-
    router's helper ui-sref to change what is being put into the
    parent's <ui-view> tag.
    
    The click _should_ be sending display information for the url to
    have params, but it's not currently working. Buttons toggle between
    views just fine.

--- app/src/components/buttons/buttons.js
+++ app/src/components/buttons/buttons.js
@@ -1,29 +1,14 @@
-import template from './image-app.html';
-import style from './image-app.scss';
+import template from './buttons.html';
+import styles from './buttons.scss';
 
 export default {
   template,
-  controller
+  controller,
+  bindings: {
+    display: '='
+  }
 };
 
-controller.$inject = ['galleryService'];
-
-function controller(galleryService) {
-  this.style = style;
-  this.view = '';
-
-  galleryService
-    .getAlbums()
-    .then(images => {
-      this.images = images;
-    });
-
-  this.remove = function(id) {
-    galleryService.removeImage(id)
-      .then(removed => {
-        const index = this.images.indexOf(removed);
-        if (index > -1) this.images.splice(index, 1);
-      });
-  };
-
+function controller() {
+  this.styles = styles;
 }
