396b1a6d3ce637fcc946db6c40d4973243222001
Gloria Guy <totallygloria@gmail.com>
2016-12-24 16:57:19 -0800

    Removes galleryService from component as it wasn't doing anything
    I was under the impression that you could put the functionality on
    the parent state and have the child states get access through it
    via explicit bindings. This wouldn't work in any incantation for
    the delete. Moved it out of the parent into the child.
    
    Delete works, but doesn't dynamically update the page, so it doesn't
    seem to work.

--- app/src/components/album/album.js
+++ app/src/components/album/album.js
@@ -2,24 +2,8 @@ import template from './album.html';
 
 export default {
   template,
-  controller,
   bindings: {
     images: '=',
     id: '<'
   }
 };
-
-controller.$inject = ['galleryService'];
-
-function controller(galleryService) {
-
-  this.remove = function(imageId) {
-    galleryService.removeImage(imageId)
-      .then(removed => {
-        const index = this.images.indexOf(removed);
-        if (index > -1) this.images.splice(index, 1);
-        console.log('Deleted: ', removed);
-      });
-  };
-
-}
