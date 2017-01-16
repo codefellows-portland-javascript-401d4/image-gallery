f36168123c05db3c6c0cae67426a9e307a32d3de
caraldi <araldicm@Caitlin-M-Araldi.local>
2017-01-16 00:13:26 -0800

    Album model

---
+++ server/lib/models/model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+
+  cover: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Album', schema);
